51320f377fde8d0f7e97567701035e161366f591
vadymshukr <vadymshukr@gmail.com>
2020-12-02 12:25:48 +0200

    Smooth scroll fixed and css for scrollbar added

--- src/assets/scripts/gulp-modules/index.js
+++ src/assets/scripts/gulp-modules/index.js
@@ -2,6 +2,39 @@
 
 (function ($) {
 
+	const locoScroll = new LocomotiveScroll({
+		el: document.querySelector(".js-scroll-container"),
+		smooth: true,
+		smoothMobile: false,
+		inertia: 1.1
+		});
+
+	// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+	ScrollTrigger.scrollerProxy(".js-scroll-container", {
+		scrollTop(value) {
+			return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+		}, // we don't have to define a scrollLeft because we're only scrolling vertically.
+		getBoundingClientRect() {
+			
+			return {
+				top: 0,
+				left: 0,
+				width: window.innerWidth,
+				height: window.innerHeight
+			};
+		},
+		// LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+		pinType: document.querySelector(".js-scroll-container").style.transform ? "transform" : "fixed"
+		});
+
+		locoScroll.on("scroll", () => {
+			ScrollTrigger.update()
+		});
+
+		ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+		ScrollTrigger.refresh();
+
 	(function() {
 
 		$('.js-header__phone').click(e => {
@@ -346,12 +379,7 @@
 					$('.gallery__preview').remove()
 					
 					changeHeaderStyles(false)
-					const locoScroll = new LocomotiveScroll({
-						el: document.querySelector(".js-scroll-container"),
-						smooth: true,
-						smoothMobile: false,
-						inertia: 1.1
-					  });
+
 
 					try {
 						$('.gallery__preview-content-show').hide()
